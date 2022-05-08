542a5b99c177825623ddef6b0ec6a5808c768ff4
rachel.szenberg@gmail.com <rachel.szenberg@gmail.com>
2022-05-08 11:23:50 -0300

    melhorando a interface do tabuleiro

--- Project1/Project1/Tabuleiro.cpp
+++ Project1/Project1/Tabuleiro.cpp
@@ -3,24 +3,51 @@
 
 #include "Tabuleiro.h"
 
+void Tabuleiro::desenhaQuadrado(int i, int j, GLubyte red, GLubyte green, GLubyte blue) {
+	glColor3ub(red, green, blue);
+	glBegin(GL_QUADS);
+		glVertex2f(i * TAM_QUADRADO, j * TAM_QUADRADO);
+		glVertex2f((i + 1) * TAM_QUADRADO, j * TAM_QUADRADO);
+		glVertex2f((i + 1) * TAM_QUADRADO, (j + 1) * TAM_QUADRADO);
+		glVertex2f(i * TAM_QUADRADO, (j + 1) * TAM_QUADRADO);
+	glEnd();
+}
+
 void Tabuleiro::Display() {
 	glLineWidth(5.0);
-	glClearColor(1, 1, 1, 0); //define qual cor vai pintar a tela
+	glClearColor(217.0f/255.0f, 199.0f / 255.0f, 159.0f / 255.0f, 0); //define qual cor vai pintar a tela
 	glMatrixMode(GL_PROJECTION);
-	// for setting the transformation which here is 2D
-	gluOrtho2D(0, 900, 0, 900);
-	glClear(GL_COLOR_BUFFER_BIT); //pinta com a dor do glClearColor(.....)
+	glLoadIdentity();
+	gluOrtho2D(0, (9 * TAM_QUADRADO), 0, (9 * TAM_QUADRADO));
+	glClear(GL_COLOR_BUFFER_BIT); //pinta com a cor do glClearColor(.....)
+	for (int i = 3; i < 6; i++) {
+		desenhaQuadrado(0, i, 120, 78, 47);
+		desenhaQuadrado(8, i, 120, 78, 47);
+		desenhaQuadrado(i, 0, 120, 78, 47);
+		desenhaQuadrado(i, 8, 120, 78, 47);
+	}
+	desenhaQuadrado(1, 4, 120, 78, 47);
+	desenhaQuadrado(7, 4, 120, 78, 47);
+	desenhaQuadrado(4, 1, 120, 78, 47);
+	desenhaQuadrado(4, 7, 120, 78, 47);
+	desenhaQuadrado(4, 4, 120, 78, 47);
+
+	for (int i = 2; i < 4; i++) {
+		desenhaQuadrado(i, 4, 214, 135, 78);
+		desenhaQuadrado(i + 3, 4, 214, 135, 78);
+		desenhaQuadrado(4, i, 214, 135, 78);
+		desenhaQuadrado(4, i + 3, 214, 135, 78);
+	}
+
 	glColor3f(0, 0, 0);
 	glBegin(GL_LINES);
-	for (int x = 0; x <= 900; x += 100)
-	{
+	for (int x = 0; x <= (9 * TAM_QUADRADO); x += TAM_QUADRADO) {
 		glVertex2f(x, 0);
-		glVertex2f(x, 900);
+		glVertex2f(x, (9 * TAM_QUADRADO));
 	}
-	for (int y = 0; y <= 900; y += 100)
-	{
+	for (int y = 0; y <= (9 * TAM_QUADRADO); y += TAM_QUADRADO) {
 		glVertex2f(0, y);
-		glVertex2f(900, y);
+		glVertex2f((9 * TAM_QUADRADO), y);
 	}
 	glEnd();
 
--- Project1/Project1/Tabuleiro.h
+++ Project1/Project1/Tabuleiro.h
@@ -1,6 +1,9 @@
 #pragma once
+#define TAM_QUADRADO 100
 class Tabuleiro
 {
+private:
+	static void desenhaQuadrado(int i, int j, GLubyte red, GLubyte green, GLubyte blue);
 public:
 	Tabuleiro() {}
 	static void Display();
